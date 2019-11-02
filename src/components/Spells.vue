0b05173f6b6a06e3bd8602ff9bc01c37a214aa02
Celeo <celeodor@gmail.com>
2019-11-02 13:44:48 -0700

    Adding spell info

--- src/components/Spells.vue
+++ src/components/Spells.vue
@@ -15,26 +15,116 @@
       <div class="row">
         <div class="col s4 m2">
           <ul class="normal-ul push-right">
-            <li>Burning Hands</li>
-            <li>Color Spray</li>
-            <li>Detect Magic</li>
-            <li>Find Familiar</li>
-            <li>Fog Cloud</li>
-            <li>Shield</li>
-            <li>Grease</li>
-            <li>Witch Bolt</li>
+            <li class="spellLink purple-text text-lighten-3" @click="triggerPopup('Burning Hands')">Burning Hands</li>
+            <li class="spellLink purple-text text-lighten-3" @click="triggerPopup('Color Spray')">Color Spray</li>
+            <li class="spellLink purple-text text-lighten-3" @click="triggerPopup('Detect Magic')">Detect Magic</li>
+            <li class="spellLink purple-text text-lighten-3" @click="triggerPopup('Find Familiar')">Find Familiar</li>
+            <li class="spellLink purple-text text-lighten-3" @click="triggerPopup('Fog Cloud')">Fog Cloud</li>
+            <li class="spellLink purple-text text-lighten-3" @click="triggerPopup('Shield')">Shield</li>
+            <li class="spellLink purple-text text-lighten-3" @click="triggerPopup('Grease')">Grease</li>
+            <li class="spellLink purple-text text-lighten-3" @click="triggerPopup('Witch Bolt')">Witch Bolt</li>
           </ul>
         </div>
-        <div class="col s4 m2">
+        <div class="col s4 m2" href="#spellModal">
           <p><b>2nd level</b></p>
           <ul class="normal-ul push-right">
-            <li>Invisibility</li>
-            <li>Web</li>
-            <li>Scorching Ray</li>
-            <li>Enlarge/Reduce</li>
+            <li class="spellLink purple-text text-lighten-3" @click="triggerPopup('Invisibility')">Invisibility</li>
+            <li class="spellLink purple-text text-lighten-3" @click="triggerPopup('Web')">Web</li>
+            <li class="spellLink purple-text text-lighten-3" @click="triggerPopup('Scorching Ray')">Scorching Ray</li>
+            <li class="spellLink purple-text text-lighten-3" @click="triggerPopup('Enlarge/Reduce')">Enlarge/Reduce</li>
           </ul>
         </div>
       </div>
     </div>
+    <div id="spellModal" class="modal">
+      <div class="modal-content cyan darken-3 white-text">
+        <h4>{{ selectedSpell }}</h4>
+        <div class="row">
+          <div class="col s12 m4">
+            <table class="table">
+              <tbody>
+                <tr>
+                  <th>Level</th>
+                  <td>{{ selectedSpellInfo.level }}</td>
+                </tr>
+                <tr>
+                  <th>School</th>
+                  <td>{{ selectedSpellInfo.school }}</td>
+                </tr>
+                <tr>
+                  <th>Time</th>
+                  <td>{{ selectedSpellInfo.time }}</td>
+                </tr>
+                <tr>
+                  <th>Range</th>
+                  <td>{{ selectedSpellInfo.range }}</td>
+                </tr>
+                <tr>
+                  <th>Components</th>
+                  <td>{{ selectedSpellInfo.components }}</td>
+                </tr>
+                <tr>
+                  <th>Duration</th>
+                  <td>{{ selectedSpellInfo.duration }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="col s12 m8">
+            <p style="font-size: 1.2em;">{{ selectedSpellInfo.description }}</p>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer cyan darken-1">
+        <a href="#!" class="modal-close btn-flat white-text">Close</a>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+// TODO
+// import caracterData from '@/characterData'
+
+import spellData from '@/spellData.json'
+import { Modal } from 'materialize-css/dist/js/materialize.min.js'
+
+export default {
+  data () {
+    return {
+      selectedSpell: null
+    }
+  },
+  computed: {
+    selectedSpellInfo () {
+      return spellData[this.selectedSpell] || {
+        level: 'unknown',
+        school: 'unknown',
+        time: 'unknown',
+        range: 'unknown',
+        components: 'unknown',
+        duration: 'unknown',
+        description: 'unknown'
+      }
+    }
+  },
+  mounted () {
+    const ref = document.querySelector('#spellModal')
+    Modal.init(ref)
+  },
+  methods: {
+    triggerPopup (spell) {
+      this.selectedSpell = spell
+      const ref = document.querySelector('#spellModal')
+      const modal = Modal.getInstance(ref)
+      modal.open()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.spellLink {
+  cursor: pointer;
+}
+</style>
